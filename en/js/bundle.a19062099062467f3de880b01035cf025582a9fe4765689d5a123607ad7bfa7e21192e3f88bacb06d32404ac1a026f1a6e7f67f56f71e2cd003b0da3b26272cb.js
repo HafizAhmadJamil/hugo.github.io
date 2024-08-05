1d56da3ffb482d029141f1a2e13d2c88975111a7
HafizAhmadJamil <iaqiddiya@gmail.com>
2024-08-05 02:47:20 +0000

    update base url

--- en/js/bundle.a19062099062467f3de880b01035cf025582a9fe4765689d5a123607ad7bfa7e21192e3f88bacb06d32404ac1a026f1a6e7f67f56f71e2cd003b0da3b26272cb.js
+++ en/js/bundle.a19062099062467f3de880b01035cf025582a9fe4765689d5a123607ad7bfa7e21192e3f88bacb06d32404ac1a026f1a6e7f67f56f71e2cd003b0da3b26272cb.js
@@ -10,7 +10,7 @@ const imageAltClass = 'img_alt';
 
 
 const defaultSiteLanguage = '';
-const baseURL = '';
+const baseURL = 'https://hafizahmadjamil.github.io/hugo.github.io';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
